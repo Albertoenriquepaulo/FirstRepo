8c44a62479895914c027a9a2a3101e04a8c8386c
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2019-12-16 17:36:52 +0100

    157. Como crear módulos JavaScript

---
+++ 157_ModulosECMA6/js/app.js
@@ -0,0 +1,11 @@
+import { nombreCliente, ahorro } from './cliente.js';
+
+// Si hay muchas variables que importar se puede ahcer de esta forma
+// client es un alias del objeto que contendrá todas las variables definidas en cliente.js
+import * as client from './cliente.js';
+
+
+console.log(nombreCliente);
+console.log(ahorro);
+console.log(client);
+// console.log(mostrarInfo('Alberto Paulo', 1000));
---
+++ 157_ModulosECMA6/js/cliente.js
@@ -0,0 +1,5 @@
+export const nombreCliente = 'Alberto'
+export let ahorro = 200;
+function mostrarInfo(nombre, ahorro) {
+    return `Cliente: ${nombre} - Ahorro : ${ahorro} `;
+}
