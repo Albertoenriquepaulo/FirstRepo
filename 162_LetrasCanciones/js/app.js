a84c6878adb6bc29ac81bef5a134322f8cd7f396
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2019-12-16 23:38:07 +0100

    164. Validando el formulario de búsqueda

--- 162_LetrasCanciones/js/app.js
+++ 162_LetrasCanciones/js/app.js
@@ -1,2 +1,21 @@
 import * as UI from "./interfaz.js";
-console.log(UI);
+
+UI.formBuscar.addEventListener('submit', e => {
+     e.preventDefault();
+     // Obtener datos del formulario, lo hacemos aqui porque si lo hacemos en la clase Interfaz las obtendremos vacias
+     // Se recomienda exportar solo lo estático
+     const artista = document.getElementById('artista').value,
+           cancion = document.getElementById('cancion').value;
+     if (artista === '' || cancion === '') {
+          //TODO, no hacer nada, mostrar mensaje de error
+          UI.divMensajes.classList.add('error');
+          UI.divMensajes.innerHTML = `Error... Todos los campos son obligatorios`;
+          setTimeout(() => {
+               UI.divMensajes.innerHTML = '';
+               UI.divMensajes.remove();
+          }, 2000);
+          } else {
+          //TODO: hacer la consulta a la API
+     }
+
+});
