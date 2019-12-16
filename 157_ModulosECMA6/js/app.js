3c518a299161114e1893d8560dfdeaf08cd75508
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2019-12-16 23:08:31 +0100

    161. Importar / Exportar de una clase que hereda de otra

--- 157_ModulosECMA6/js/app.js
+++ 157_ModulosECMA6/js/app.js
@@ -1,7 +1,7 @@
 import { nombreCliente, ahorro, mostrarInfo, Cliente } from './cliente.js';
 //Como ahorro existe en los dos JS, podemos hacer lo siguiente para dfierenciarlos usando "as"
 // igual con mostrarInfo
-import { nombreEmpresa, ahorro as ahorroEmpresa, categoria, mostrarInfo as mostrarInfoEmpresa } from './empresa.js';
+import { nombreEmpresa, ahorro as ahorroEmpresa, categoria, mostrarInfo as mostrarInfoEmpresa, Empresa } from './empresa.js';
 
 // Si hay muchas variables que importar se puede ahcer de esta forma
 // client es un alias del objeto que contendrá todas las variables definidas en cliente.js
@@ -25,3 +25,7 @@ let cliente = new Cliente(nombreCliente, ahorro);
 console.log(cliente);
 console.log(cliente.mostrarInfo()); 
 
+// Ya importamos la clase Empresa, que a su vez hereda de Cliente, ahora declaramos una instancia para hacer pruebas
+let empresa = new Cliente(nombreEmpresa, ahorroEmpresa, categoria );
+console.log(empresa);
+console.log(empresa.mostrarInfo()); 
--- 157_ModulosECMA6/js/empresa.js
+++ 157_ModulosECMA6/js/empresa.js
@@ -1,3 +1,5 @@
+import { Cliente } from "./cliente.js";
+
 // Exportar variables
 export const nombreEmpresa = 'Udemy'
 export let ahorro = 200000000;
@@ -7,4 +9,15 @@ export const categoria = 'Aprendizaje'
 // Las funciones son variables en JS por lo tanto se imprtan de igual forma
 export function mostrarInfo(nombre, ahorro) {
     return `Cliente: ${nombre} - Ahorro : ${ahorro} - Categoria: ${nombreEmpresa}`;
+}
+
+// utilizar la clase del modulo cliente
+export class Empresa extends Cliente{
+    constructor(nombre, ahorro, categoria){
+        super(nombre, ahorro);  //dejamos el mismo codigo heredado de la clase Cliente, solo modificamos categoria, que no está en aquella clase
+        this.categoria = categoria;
+    }
+    mostrarInfo() {
+        return `Cliente: ${this.nombre} - Ahorro: ${this.ahorro} - Categoria: ${this.categoria}`;
+    }
 }
