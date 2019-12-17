4cf98f6eb0477da14e1c8ef1a0288e95a4d38eca
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2019-12-17 01:32:40 +0100

    169. Module - Module Pattern ( 2 de 2 )

--- 168_PatronesJS/js/module.js
+++ 168_PatronesJS/js/module.js
@@ -1,14 +1,23 @@
 const comprarBoleto = (function() {
     // Privado, lo que está fuera del return es privado, no puede ser accedido fuera del programa
     let evento = 'Coneferencia JS 2019';
-    let precio = 200;
+    const adquirirBoleto = () =>{
+        const elemento = document.createElement('p');
+        elemento.textContent = `Comprando Boleto para: ${evento}`;
+        document.querySelector('#app').appendChild(elemento);
+    }
 
     // Publico
     return {
         mostrarBoleto: function () {
-            console.log(evento);
+            adquirirBoleto();
+        },
+
+        mostrarBoleto1: () => {
+            adquirirBoleto();
         }
     }
 })();
 
 comprarBoleto.mostrarBoleto();
+comprarBoleto.mostrarBoleto1();
