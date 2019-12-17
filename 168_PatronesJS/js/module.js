e7e27e5a4add87f0bf04fc6e2509679ba508bba6
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2019-12-17 13:10:06 +0100

    Clases con recursos privados y publicos JS

--- 168_PatronesJS/js/module.js
+++ 168_PatronesJS/js/module.js
@@ -1,3 +1,4 @@
+// Forma de JS de hacer clases con recursos Privados y Públicos
 const comprarBoleto = (function() {
     // Privado, lo que está fuera del return es privado, no puede ser accedido fuera del programa
     let evento = 'Coneferencia JS 2019';
