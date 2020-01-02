d5d9e518020d6193684fc19013d3a2aa344adbc9
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-01-03 00:02:31 +0100

    199. Creando la base de datos

--- 196_IndexedDB/js/app.js
+++ 196_IndexedDB/js/app.js
@@ -10,3 +10,22 @@ const form = document.querySelector('form'),
         sintomas = document.querySelector('#sintomas'),
         citas = document.querySelector('#citas'),
         headingAdministra = document.querySelector('#administra');
+
+// Esperar por el DOM DOMMatrixReadOnly, IndexDB requiere que este cargado el DOM
+document.addEventListener('DOMContentLoaded', () => {
+    // Crear la DB
+    let crearDB = window.indexedDB.open('citas', 1);
+
+    // Si hay un error, enviarlo a la consola
+    crearDB.onerror = function () {
+        console.log('Hubo un error');
+    }
+
+    // si todo esta bien mostrar en consola y asignar la DB
+    crearDB.onsuccess = function () {
+        console.log('Todo listo');
+        // Asignar a la base de datos
+        DB = crearDB.result;
+        console.log(DB);
+    }
+});
