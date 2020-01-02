163045d7fe9f7846acaa3dd5a88c5211fff672b0
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-01-02 18:53:59 +0100

    180. NameSpace (2 de 2)

--- 168_PatronesJS/js/namespace.js
+++ 168_PatronesJS/js/namespace.js
@@ -3,6 +3,7 @@
 // funciones y metodos en el mismo lugar que se puedan acceder de forma global
 // La idea es ques e global pero que solo sea accedido por medio de un unico objeto
 // en este caso restaurApp
+// JQuery es un ejemplo de esto
 const restaurApp = {};
 
 restaurApp.platillos = [
@@ -24,9 +25,25 @@ restaurApp.platillos = [
 restaurApp.funciones = {
     ordenar: id => {
         console.log(`Tu platillo ${restaurApp.platillos[id].platillo} se está preparando`);
+    },
+
+    agregarPlatillo: (platillo, precio) => {
+        const nuevo = {
+            platillo,
+            precio
+        }
+        restaurApp.platillos.push(nuevo);
+    },
+    
+    mostrarMenu: () => {
+        restaurApp.platillos.forEach( (platillo, index)=> {
+            console.log(`${index}: ${platillo.platillo} -> $${platillo.precio}`);
+        });
     }
 } 
 
 
 console.log(restaurApp);
 restaurApp.funciones.ordenar(2);
+restaurApp.funciones.agregarPlatillo('Sandwich', 5);
+restaurApp.funciones.mostrarMenu();
