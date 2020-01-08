19836c09d74ae9670f5c1cd67c6e0b660176c5cb
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-01-08 13:50:01 +0100

    233. Importando SCSS en JS. ¿Como importar un archivo SASS en nuestros modulos?

--- Aprendiendo-webpack/src/js/index.js
+++ Aprendiendo-webpack/src/js/index.js
@@ -1,7 +1,14 @@
-import '../css/style.css';
+// Para que esta linea de abajo funcione hay que instalar los loaders necesarios
+// En este caso:
+//     npm install --save-dev style-loader css-loader
+// Esta linea hace que se aplique el CSS sin necesidad de linkearlo en el HTML
+// Esto no se puede hacer con JS FocusNavigationEvent, pero si con Webpack
+import '../css/style.scss';
+// si queremos importar bootstrap o material fundación o material design se hace de 
+// esta forma escrita arriba, con import
 
 class Cliente {
-    constructor(name){
+    constructor(name) {
         this.name = name;
     }
 }
--- Aprendiendo-webpack/webpack.config.js
+++ Aprendiendo-webpack/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/js/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.join(__dirname, '/dist') 
+        path: path.join(__dirname, '/dist')
     },
     module: {
         rules: [
@@ -18,8 +18,16 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    {loader: 'style-loader'},
-                    {loader: 'css-loader'}
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' }
                 ]
             }
         ]
